fbef6f6df875dc2b5ccae4dec4a8ba9d76c8f3d7
krishnakumbhaj <krishnakumbhaj@gmail.com>
2024-09-04 16:32:48 +0530

    code

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import the CORS package
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user')
+
+// express app
+const app = express();
+
+// Use CORS middleware
+app.use(cors()); // Enable CORS for all routes
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes)
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database');
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
